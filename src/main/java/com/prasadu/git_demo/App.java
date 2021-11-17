e98a26d4b56fa38617495f90e10da11c632ddcd6
gprasadu12 <gprasadu04@gmail.com>
2021-11-17 18:31:47 +0530

    added if condition

--- src/main/java/com/prasadu/git_demo/App.java
+++ src/main/java/com/prasadu/git_demo/App.java
@@ -7,7 +7,9 @@ package com.prasadu.git_demo;
 public class App 
 {
 	public void addData(String data) {
+		if(data.equals("welcome")) {
 		System.out.println("Data="+data);
+		}
 	}
     public static void main( String[] args )
     {
